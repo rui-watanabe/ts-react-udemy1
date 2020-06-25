c46d6e242c5b78e2aeeb180e71ab665e0196358b
rui-watanabe <wataru610806@gmail.com>
2020-06-25 22:13:43 +0900

    set webpack and babel

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default function App(): JSX.Element{
+  const sum = (a: number, b: number): number => a + b;
+  const numString: string = `Number ${ sum(1, 2) }`;
   return(
-    <div>
-      Hello
-    </div>
+    <h1>
+      { numString }
+    </h1>
   )
 };
 
