df406d273f4e2ca82015398d918f01f3f7f89932
rui-watanabe <wataru610806@gmail.com>
2020-06-26 21:39:19 +0900

    create form and define type

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+type formElement = React.FormEvent<HTMLFormElement>;
+
 export default function App(): JSX.Element{
-  const sum = (a: number, b: number): number => a + b;
-  const numString: string = `Number ${ sum(1, 2) }`;
+  const [value, setValue] = useState<string>('');
+
+  const handleSubmit = (e: formElement): void => {
+    e.preventDefault();
+    setValue('');
+  }
+
   return(
-    <h1>
-      { numString }
-    </h1>
+    <>
+      <h1>Todo List</h1>
+      <form onSubmit={ handleSubmit }>
+        <input type="text" value={ value } onChange={e => setValue(e.target.value)} required />
+        <button type="submit">Add Todo</button>
+      </form>
+    </>
   )
 };
 
